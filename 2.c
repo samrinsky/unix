dd2c277cc4c496af5dc5f8011d0f4c55e3e5758f
samrinsky <116232304+samrinsky@users.noreply.github.com>
2022-10-20 11:50:31 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -45,3 +45,60 @@ int main()
                 execlp("wc", "wc", "-l", NULL);
         }
 }
+
+
+//or
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+#define READ 0
+#define WRITE 1
+
+int main(){
+        int fd1[2], fd2[2];
+        int forkresult;
+
+        if(pipe(fd1)==-1 || pipe(fd2)==-1){
+                perror("pipes could not be created");
+                exit(1);
+        }
+//      perror("Pipes created");
+        int log = open("log", O_RDONLY);
+
+        int i;
+        char buf[1];
+        while(i=(read(log,buf,1))>0)
+        {
+                write(fd1[WRITE], buf, 1);
+        }
+//      perror("wrote file: about to fork");
+        forkresult = fork();
+        if(forkresult <0){
+//              perror("fork failed");
+                exit(2);
+        }
+
+        if(forkresult==0){
+//              perror("in child");
+                dup2(fd1[READ], READ);
+                dup2(fd2[WRITE], WRITE);
+//              perror("before execl");
+                execlp("grep", "grep", "admin",NULL);
+        }
+        else{
+//              perror("before wait");
+                wait(NULL);
+//              perror("after wait");
+                close(fd1[READ]);
+                close(fd2[WRITE]);
+                dup2(fd2[READ], READ);
+//              dup2(stdout, WRITE);
+//              perror("before execlp");
+                execlp("wc", "wc", "-l", NULL);
+        }
+}
