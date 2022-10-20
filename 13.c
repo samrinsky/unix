e768fbe283a456d5b42ff20d54a4f80362eafb50
samrinsky <116232304+samrinsky@users.noreply.github.com>
2022-10-20 06:12:17 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,25 +1,37 @@
 int n;
-binary_semaphore s = 1, delay = 0; void producer()
+binary_semaphore s = 1, delay = 0; 
+
+void producer()
 {
-while (true) { produce();
-                semWaitB(s);
-                append();
-                n++;
-                if (n==1) semSignalB(delay);
-                semSignalB(s);
-} }
-void consumer() {
-int m; /* a local variable */ semWaitB(delay);
-while (true) {
-} }
-void main() {
+  while (true) 
+  { 
+    produce();
+    semWaitB(s);
+    append();
+    n++;
+    if (n==1) semSignalB(delay);
+    semSignalB(s);
+  }
 }
-semWaitB(s);
-take();
-n--;
-m = n;
-semSignalB(s);
-consume();
-if (m==0) semWaitB(delay);
-n = 0;
-parbegin (producer, consumer);
+
+void consumer() 
+{
+  int m; /* a local variable */ 
+  semWaitB(delay);
+  while (true) 
+  {
+    semWaitB(s);
+    take();
+    n--;
+    m = n;
+    semSignalB(s);
+    consume();
+    if (m==0) semWaitB(delay);
+  }
+}
+
+  
+void main(){ 
+  n = 0;
+  parbegin (producer, consumer);
+}
